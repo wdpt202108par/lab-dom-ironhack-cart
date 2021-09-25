0fc3ed6e6b357109ed27861141beb2be41995a96
Auriane Viggiano <auriane.vigg@gmail.com>
2021-09-25 11:37:23 +0200

    itération 4

--- js/index.js
+++ js/index.js
@@ -10,18 +10,28 @@ function updateSubtotal(product) {
   // console.log(SubTotal);
 
   product.querySelector('.subtotal span').textContent = SubTotal;
+
+  return SubTotal;
 }
 
 function calculateAll() {
   var products = document.getElementsByClassName('product');
 
+  var total = 0;
+
   // ITERATION 2
   for (let product of products) {
     updateSubtotal(product);
+
+    total += updateSubtotal(product);
+
   }
 
   // ITERATION 3
   //... your code goes here
+  document.querySelector('#total-value span').textContent = total;
+
+
 }
 
 // ITERATION 4
@@ -30,6 +40,14 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  
+  let tbody = document.querySelector('#cart tbody');
+
+  let theTr = target.parentNode.parentNode;
+
+
+  tbody.removeChild(theTr);
+
 }
 
 // ITERATION 5
@@ -43,4 +61,9 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
+  let btn = document.querySelectorAll('.btn-remove');
+
+  for (let element of btn) {
+      element.addEventListener('click', removeProduct);
+} 
 });
